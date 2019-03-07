a0b8062d88c7664d6dd385864b91c4bada09462b
Glitch (hello-express) <none>
2019-03-07 23:55:44 +0000

    💊👌 Checkpoint ./server.js:3109138/19

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var marvel = api.createClient({
 
 app.get('/marvel', (request, response) => {
  let data;
-  marvel.characters.findByName('spider-man')
+  marvel.characters.findByName('daredevil')
   .then(
   r => {
    response.send(r.data); 
