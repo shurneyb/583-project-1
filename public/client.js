af9decec129c9c535264bacb3bbd93ce3475bbed
Glitch (hello-express) <none>
2019-03-07 23:15:12 +0000

    🌖🚪 Checkpoint ./public/client.js:3109138/2

--- public/client.js
+++ public/client.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   fetch('/marvel').then(resp => resp.json()).then((data) => {
     var char= []; 
-    char.push(data[0]);
+    char.push(data[1]);
     console.log(char);
   });
       
