7c47014ec20c3665b10397ced7148d4e1da41720
Glitch (hello-express) <none>
2019-03-08 02:58:00 +0000

    🕶🌪 Checkpoint ./public/client.js:3109138/88

--- public/client.js
+++ public/client.js
@@ -5,6 +5,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log("DOM fully loaded and parsed");
   console.log('scroll magic is working!', ScrollMagic);
+  var controller = new ScrollMagic.Controller();
+  var scene = new ScrollMagic.Scene();
 
   fetch('/marvel').then(resp => resp.json()).then((data) => {
     console.log(data);
